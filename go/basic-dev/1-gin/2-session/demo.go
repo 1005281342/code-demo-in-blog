aab797dab2536ad2acba778f10ec3619494f35b1
1005281342 <1005281342@qq.com>
2024-08-27 00:56:35 +0800

    gin register user demo

---
+++ go/basic-dev/1-gin/2-session/demo.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	server := gin.Default()
+
+	db, err := gorm.Open(sqlite.Open("user.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	_ = db.AutoMigrate(&User{})
+
+	var u = &user{
+		db: db,
+	}
+
+	ug := server.Group("/users")
+	// POST /users/signup
+	ug.POST("/signup", u.SignUp)
+
+	server.Run(":8080")
+}
+
+type user struct {
+	db *gorm.DB
+}
+
+func (u *user) SignUp(ctx *gin.Context) {
+	type SignUpReq struct {
+		Email           string `json:"email"`
+		Password        string `json:"password"`
+		ConfirmPassword string `json:"confirmPassword"`
+		Name            string `json:"name"`
+	}
+
+	var req SignUpReq
+	if err := ctx.Bind(&req); err != nil {
+		return
+	}
+
+	if req.Password != req.ConfirmPassword {
+		ctx.String(http.StatusOK, "两次输入密码不对")
+		return
+	}
+
+	if len(req.Name) > 8 {
+		ctx.String(http.StatusOK, "昵称不得超过8个字符")
+		return
+	}
+
+	// mock register user ...
+	var timeNowUnixMilli = time.Now().UnixMilli()
+	var err = u.db.Model(&User{}).Create(&User{
+		Name:      req.Name,
+		Email:     req.Email,
+		Password:  req.Password,
+		CreatedAt: timeNowUnixMilli,
+		UpdatedAt: timeNowUnixMilli,
+	}).Error
+	if err != nil {
+		ctx.String(http.StatusOK, "注册失败, "+err.Error())
+		return
+	}
+
+	ctx.String(http.StatusOK, "注册成功")
+}
---
+++ go/basic-dev/1-gin/2-session/user.go
@@ -0,0 +1,17 @@
+package main
+
+import "gorm.io/gorm"
+
+type User struct {
+	ID        int64          `gorm:"primarykey,column:id"`
+	Name      string         `gorm:"column:name"`
+	Email     string         `gorm:"unique,column:email"`
+	Password  string         `gorm:"column:password"`
+	CreatedAt int64          `gorm:"column:created_at"`
+	UpdatedAt int64          `gorm:"column:updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index,column:deleted_at"`
+}
+
+func (u *User) TableName() string {
+	return "t_users"
+}
